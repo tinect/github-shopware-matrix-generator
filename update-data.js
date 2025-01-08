5be220f952d1aa9cfaa3374507f446ef8d42891b
tinect <s.koenig@tinect.de>
2025-01-08 15:17:58 +0100

    feat: transform results of external files to own format

--- update-data.js
+++ update-data.js
@@ -1,23 +1,93 @@
 const fs = require("node:fs");
+const semver = require('semver');
 
-const files = [
-    {
-        link: 'https://raw.githubusercontent.com/FriendsOfShopware/shopware-static-data/refs/heads/main/data/all-supported-php-versions-by-shopware-version.json',
-        name: 'all-supported-php-versions-by-shopware-version.json'
-    },
-    {
-        link: 'https://php.watch/api/v1/versions',
-        name: 'php-versions.json'
-    },
-    {
-        link: 'https://raw.githubusercontent.com/shopware/shopware/refs/heads/trunk/releases.json',
-        name: 'shopware-releases.json'
-    },
-];
+async function getPhpVersions() {
+    const phpVersions = await fetchJson('https://php.watch/api/v1/versions');
 
-files.forEach(file => {
-    fetch(file.link)
-        .then(response => response.json())
-        .then(data => fs.writeFileSync(`data/${file.name}`, JSON.stringify(data, null, 2)));
-});
+    return Object.values(phpVersions.data).map(function (version) {
+        return {
+            version: version.name,
+            eol: version.eolDate
+        };
+    });
+}
 
+function cleanVersion(version) {
+    const parts = version.split('.').slice(0, 3);
+    if (parts.length === 2) {
+        parts.push('0');
+    }
+
+    return parts.join('.');
+}
+
+async function getShopwareRelease(version) {
+    const shopwareReleases = await fetchJson('https://raw.githubusercontent.com/shopware/shopware/refs/heads/trunk/releases.json');
+
+    const releases = shopwareReleases.filter(function (release) {
+        return semver.gte(cleanVersion(version), cleanVersion(release.version));
+    });
+
+    const release = releases[releases.length - 1];
+
+    if (!release) {
+        return {
+            security_eol: '1900-01-01',
+        };
+    }
+
+    return {
+        security_eol: release.security_eol,
+    };
+}
+
+async function updateData() {
+    const data = require('./src/data.json');
+    const shopwareVersions = await fetchJson('https://raw.githubusercontent.com/FriendsOfShopware/shopware-static-data/refs/heads/main/data/all-supported-php-versions-by-shopware-version.json')
+
+    const phpVersions = await getPhpVersions();
+
+    const shopwareMinorData = {};
+    for (const [shopwareVersion, supportedPhpVersions] of Object.entries(shopwareVersions)) {
+        let shopwareMinor = shopwareVersion.split('.').slice(0, 3).join('.');
+
+        supportedPhpVersions.forEach((phpVersion, i) => {
+            supportedPhpVersions[i] = phpVersions.find(version => version.version === phpVersion);
+        });
+
+        shopwareMinorData[shopwareMinor] = {
+            version: shopwareVersion,
+            minor_version: shopwareMinor,
+            php_versions: supportedPhpVersions
+        };
+    }
+
+    Object.values(shopwareMinorData).forEach((element) => {
+        const i = data.findIndex((el) => {
+            return el.version === element.version;
+        });
+
+        Object.assign(element, getShopwareRelease(element.version));
+
+        if (i > -1) {
+            data[i] = {...data[i], ...element};
+            return;
+        }
+
+        data.push(element);
+    });
+
+    data.sort((a, b) => {
+        return a.version.localeCompare(b.version, undefined, {numeric: true});
+    });
+
+    fs.writeFileSync('./src/data.json', JSON.stringify(data, null, 2));
+}
+
+async function fetchJson(url) {
+    const response = await fetch(url);
+
+    return await response.json();
+}
+
+updateData().then(r => console.log('Data updated'));
